e0ce26ef7c1c705606ca2503ec5f539583274911
Gregory Peterson <pet03356@morris.umn.edu>
2023-05-02 17:08:40 -0500

    Missed some.

--- client/src/app/selected-files-table/selected-files-table.component.ts
+++ client/src/app/selected-files-table/selected-files-table.component.ts
@@ -3,7 +3,6 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-selected-files-table',
   templateUrl: './selected-files-table.component.html',
-  styleUrls: ['./selected-files-table.component.css']
 })
 export class SelectedFilesTableComponent {
   @Input()
